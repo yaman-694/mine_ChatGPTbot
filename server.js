db89bf972a63664646aebf64a71c5041d5d3e649
yaman-694 <jyaman694@gmail.com>
2023-01-04 15:12:34 +0530

    code with ai

---
+++ server.js
@@ -0,0 +1,37 @@
+const TelegramBot = require('node-telegram-bot-api');
+const dotenv = require('dotenv')
+const { Configuration, OpenAIApi } = require("openai");
+
+
+dotenv.config();
+
+const bot = new TelegramBot(process.env.TELE_TOKEN, {polling: true});
+
+const configuration = new Configuration({
+    apiKey: process.env.OPEN_AI_KEY
+    });
+
+const openai = new OpenAIApi(configuration);
+
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    const promt = msg.text;
+    console.log(promt); 
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `${promt}`,
+        temperature: 0,
+        max_tokens: 1000,
+        top_p: 1,
+        frequency_penalty: 0.2,
+        presence_penalty: 0,
+      });
+
+      bot.sendMessage(chatId, response.data.choices[0].text.trim());
+});
+
+// const text ="eyJoaXN0b3J5IjpbLTUzODg1ODYyMywtMTg4OTgzNTcyMCwxND"
+
+// const decodedText = Buffer.from(text, 'base64').toString('utf8');
+
+// console.log(decodedText);
